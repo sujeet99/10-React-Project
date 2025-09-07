f3e210c54fcea6ca2edfcbd83b7ff2d6aa458963
sujeet vishwas <sujeetvishwas24@gmail.com>
2025-09-07 07:47:03 +0530

    background image implemented

--- Project-1 Netflix clone/Netflix clone/src/Pages/Home/Home.jsx
+++ Project-1 Netflix clone/Netflix clone/src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Home.css'
 import Navbar from '../../Components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
-import hero-title from '../../assets/hero-title.png'
+import hero_title from '../../assets/hero_title.png'
 const Home = () => {
   return (
     <div className='home'>
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="hero">
         <img src={hero_banner} alt="" className='banner-img'/>
         <div className="hero-caption">
-           <img src={hero_title} alt="" />
+           <img src={hero_title} alt="" className='caption-img'/>
         </div>
 
       </div>
